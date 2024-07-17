d7cf1dc71ddf9854c7cbbbc668fc75df7c9959cc
sulaymon-tajudeen-hpe <sulaymon.tajudeen@hpe.com>
2024-07-17 11:46:13 +0100

    portfolio v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Grommet,
+  Box,
+  Header,
+  Text,
+  Card,
+  CardHeader,
+  Button,
+  CardBody,
+  Avatar,
+  Heading,
+  Paragraph,
+  CardFooter,
+  Footer,
+} from 'grommet';
+import { Home, Previous } from 'grommet-icons';
+import { hpe as theme } from 'grommet-theme-hpe';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grommet full theme={theme}>
+      <Box align='center' justify='between' fill='vertical'>
+        <Header
+          align='center'
+          direction='row'
+          flex={false}
+          justify='between'
+          gap='medium'
+          fill='horizontal'
+          pad='small'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box align='center' justify='center' direction='row' gap='small'>
+            <Home size='xlarge' color='brand' />
+            <Box align='center' justify='center' direction='row' gap='xxsmall'>
+              <Text weight='bold' color='text-strong'>
+                Student
+              </Text>
+              <Text>Portfolio</Text>
+            </Box>
+          </Box>
+          <Box
+            align='center'
+            justify='center'
+            height='xxsmall'
+            width='xxsmall'
+            round='full'
+            background={{ color: 'background-contrast' }}
+          >
+            <Text>ST</Text>
+          </Box>
+        </Header>
+        <Card pad='medium'>
+          <CardHeader
+            align='center'
+            direction='row'
+            flex={false}
+            justify='between'
+            gap='medium'
+          >
+            <Button icon={<Previous />} primary />
+          </CardHeader>
+          <CardBody pad='small' justify='center' align='center'>
+            <Avatar
+              align='center'
+              flex={false}
+              justify='center'
+              overflow='hidden'
+              round='full'
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS4Qk3aU8xKsdhRJab2mycsbnaN307baTz3lRJbaSMiw&s
+'
+              size='large'
+            />
+            <Heading margin={{ bottom: 'small' }}>Sulaymon Tajudeen</Heading>
+            <Text>Experience Design Engineer</Text>
+            <Paragraph textAlign='center'>
+              I am an Experience Design Engineer specializing in User Analytics
+              Behavior and Grommet/React at Hewlett Packard Enterprise.
+            </Paragraph>
+          </CardBody>
+          <CardFooter
+            align='center'
+            direction='row'
+            flex={false}
+            justify='center'
+            gap='medium'
+            pad='small'
+          >
+            <Button label='Porfolio' primary />
+            <Button label='Contact Me' primary={false} />
+          </CardFooter>
+        </Card>
+        <Footer
+          align='center'
+          direction='row'
+          flex={false}
+          justify='between'
+          gap='medium'
+          fill='horizontal'
+          pad='medium'
+        >
+          <Text>© 2024 Hewlett Packard Enterprise Development LP</Text>
+          <Box align='center' justify='center' direction='row' gap='small'>
+            <Button label='Terms' plain />
+            <Button label='Security' plain />
+            <Button label='Privacy' plain />
+            <Button label='Feedback' plain />
+          </Box>
+        </Footer>
+      </Box>
+    </Grommet>
   );
-}
+};
 
 export default App;
